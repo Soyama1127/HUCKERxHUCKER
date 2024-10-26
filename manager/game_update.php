7a0e9bd411b46a1e54b7c8d4f9da5f80c7418ee9
Soyama1127 <2301017@s.asojuku.ac.jp>
2024-10-26 13:28:41 +0900

    first comit

---
+++ manager/game_update.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./../css/manager.css">
+    <title>商品更新</title>
+</head>
+
+<body>
+    <header>
+        ヘッダー
+    </header>
+    <main>
+        画面
+    </main>
+</body>
+
+</html>
