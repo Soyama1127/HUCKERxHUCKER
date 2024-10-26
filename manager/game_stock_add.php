9ea11341505a44d0374e60a19799388c95c5d496
Soyama1127 <2301017@s.asojuku.ac.jp>
2024-10-26 23:52:15 +0900

    first comit

--- manager/game_stock_add.php
+++ manager/game_stock_add.php
@@ -13,7 +13,62 @@
         ヘッダー
     </header>
     <main>
-        画面
+        <div class="card-body">
+            <caption>My Favorite Album</caption>
+            <table id="fav-table" class="table table-bordered" border="1">
+                <thead>
+                    <tr>
+                        <th>Album</th>
+                        <th>Artist</th>
+                        <th>Country</th>
+                        <th>Label</th>
+                        <th>Release</th>
+                        <th>Recommend</th>
+                    </tr>
+                </thead>
+                <tr>
+                    <td>Demi Masa</td>
+                    <td>simakDialog</td>
+                    <td>Indonesia</td>
+                    <td>Moonjune Records</td>
+                    <td>2009.06</td>
+                    <td>★★★★★★★★</td>
+                </tr>
+                <tr>
+                    <td>Postaeolian Train Robbery</td>
+                    <td>CoS</td>
+                    <td>Belgium</td>
+                    <td>Plus Records</td>
+                    <td>1974.08</td>
+                    <td>★★★★</td>
+                </tr>
+                <tr>
+                    <td>organizer</td>
+                    <td>KIYO＊SEN</td>
+                    <td>Japan</td>
+                    <td>ELEC RECORDS</td>
+                    <td>2018.07</td>
+                    <td>★★★</td>
+                </tr>
+                <tr>
+                    <td>Explorations</td>
+                    <td>Bill Evans</td>
+                    <td>America</td>
+                    <td>Riverside Records</td>
+                    <td>1961.03</td>
+                    <td>★★★★★★</td>
+                </tr>
+                <tr>
+                    <td>わたしの毎日</td>
+                    <td>石川ひとみ</td>
+                    <td>Japan</td>
+                    <td>IMPERIAL RECORDS</td>
+                    <td>2018.06</td>
+                    <td>★★★★★★★</td>
+                </tr>
+            </table>
+        </div>
+        <script src="./../js/script.js"></script>
     </main>
 </body>
 
