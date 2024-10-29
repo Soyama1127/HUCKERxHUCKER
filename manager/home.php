67803fcece64f754432c7f34868ec1c8e2ea76ff
azumaden <2301024@s.asojuku.ac.jp>
2024-10-29 12:45:39 +0900

    変更中

--- manager/home.php
+++ manager/home.php
@@ -12,9 +12,22 @@
     <header>
         GAMESOYA管理者
     </header>
+    <h1>管理者画面</h1>
+    <div>
+                <input type="submit" value="商品登録"style="width:300px;height:50px"></p>
+                <input type="submit" value="商品登録"style="width:300px;height:50px"></p>
+                <form action="game_update.php" method="post"><input type="submit" value="商品更新" style="width:300px;height:50px"></form>
+    </div>
+    
+    
+                
+    
+
+    <p><form action="game_update.php" method="post">
+            <input type="submit" value="商品更新" style="width:300px;height:50px">
+    </form></p>
     <main>
-        画面
-    </main>
+</main>
 </body>
 
 </html>
