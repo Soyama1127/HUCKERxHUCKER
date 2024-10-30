d73fda7eae8f39a8ae46c0835e69c6bdcff0567c
Soyama1127 <2301017@s.asojuku.ac.jp>
2024-10-30 11:43:18 +0900

    name変更

--- user/account_setting.php
+++ user/account_setting.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./../css/user.css">
-    <title>アカウント設定</title>
+    <title>アカウント更新</title>
 </head>
 
 <body>
