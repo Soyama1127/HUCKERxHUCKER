0767ee4aa5f0ef55dd9ba910b0f32db7f0678b72
yuki2004831 <ubei138sy@gmail.com>
2024-11-01 12:25:01 +0900

    データベース完成

--- manager/signup_complete.php
+++ manager/signup_complete.php
@@ -13,6 +13,14 @@
         GAMESOYA管理者
     </header>
     <main>
+    <?php
+    $id = $_POST['manager_id'];
+    $password = $_POST['manager_password'];
+    $name = $_POST['manager_name'];
+    $pdo = new PDO('mysql:host=mysql309.phy.lolipop.lan;dbname=LAA1553864-gamesoya;', 'LAA1553864', 'Pass1127');
+    $sql = $pdo->prepare('insert into manager(manager_id,manager_password,manager_name) values(?,?,?)');
+    $sql->execute([$id, $password,$name]);
+    ?>
     <form action="login.php" method="post">
     <h1>登録完了しました</h1>
     <input type=submit value=完了>
