c073d77760ec85413f7c4dfa2215dcebbfa9f22b
yuki2004831 <ubei138sy@gmail.com>
2024-11-01 11:39:38 +0900

    一回目

--- manager/signup.php
+++ manager/signup.php
@@ -13,7 +13,16 @@
         GAMESOYA管理者
     </header>
     <main>
-        画面
+    <form action="home.php" method="post">
+        管理者登録
+        ID<br>
+        <input type=text name=manager_id><br>
+        パスワード<br>
+        <input type=text name=manager_password><br>
+        管理者名<br>
+        <input type=text name=manager_name><br>]
+        <input type=submit value=作成>
+    </form>
     </main>
 </body>
 
