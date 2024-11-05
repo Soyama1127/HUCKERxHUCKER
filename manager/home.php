aae12c967e5ef882546a4cb11aca72c58c7f5162
Soyama1127 <2301017@s.asojuku.ac.jp>
2024-11-05 14:39:38 +0900

    管理者ホーム画面のcss作成

--- manager/home.php
+++ manager/home.php
@@ -12,16 +12,51 @@
     <header>
         GAMESoya管理者
     </header>
-    <h1>管理者画面</h1>
-    <form action="login.php" method="post"><input name="0"value="ログアウト"></form>
-    <form action="game_add.php" method="post"><input name="a" type="submit" value="商品登録"style="width:300px;height:50px"></form>
-    <form action="game_update.php" method="post"><input name="b" type="submit" value="商品更新"style="width:300px;height:50px"></form>
-    <form action="game_delete.php" method="post"><input name="c" type="submit" value="商品削除"style="width:300px;height:50px"></form>
-    <form action="user_delete.php" method="post"><input name="d" type="submit" value="ユーザー削除"style="width:300px;height:50px"></form>
-    <form action="pr_approval.php" method="post"><input name="e" type="submit" value="ユーザーPR"style="width:300px;height:50px"></form>
-    <form action="game_stock_add.php" method="post"><input name="f" type="submit" value="在庫管理"style="width:300px;height:50px"></form>
+
+    <h1>ホーム</h1>
+
+    <form action="login.php" method="post" class="logout">
+
+        <input type="submit" value="ログアウト" class="logout_button">
+
+    </form>
+
+    <form action="game_add.php" method="post">
+
+        <input type="submit" value="商品登録" class="home_button">
+
+    </form>
+
+    <form action="game_update.php" method="post">
+
+        <input type="submit" value="商品更新" class="home_button">
+
+    </form>
+
+    <form action="game_delete.php" method="post">
+
+        <input type="submit" value="商品削除" class="home_button">
+
+    </form>
+
+    <form action="user_delete.php" method="post">
+
+        <input type="submit" value="ユーザー削除" class="home_button">
+
+    </form>
+
+    <form action="pr_approval.php" method="post">
+
+        <input type="submit" value="ユーザーPR" class="home_button">
+
+    </form>
+
+    <form action="game_stock_add.php" method="post">
+
+        <input type="submit" value="在庫管理" class="home_button">
+    
+    </form>
 </body>
 
 
 </html>
-
