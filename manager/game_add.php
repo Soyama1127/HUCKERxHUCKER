34635366e287c924e801740c298c945c300eea5a
Soyama1127 <2301017@s.asojuku.ac.jp>
2024-11-08 20:47:34 +0900

    ゲーム商品追加デザイン

--- manager/game_add.php
+++ manager/game_add.php
@@ -13,44 +13,43 @@
         GAMESOYA管理者
     </header>
     <main>
-    <form action="game_add_complete.php" method="post" enctype="multipart/form-data">
-        商品登録<br>
-        ゲーム名<br>
-        <input type=text name=game_name><br>
-        料金<br>
-        <input type=text name=game_price><br>
-        機種<br>
-        <select name='model'>
-        <option value='switch'>switch</option>
-        <option value='PS'>PS(プレイステーション)</option>
-        <option value='wii'>Wii</option>
-        <option value='64'>64</option>
-        <option value='Xbox'>Xbox</option>
-        <option value='famicon'>ファミコン</option>
-        <option value='other'>その他</option>
-        </select><br>
-        ジャンル<br>
-        <select name='game_genre'>
-        <option value='RPG'>RPG</option>
-        <option value='action'>アクション</option>
-        <option value='adventure'>アドベンチャー</option>
-        <option value='simulation'>シュミレーション</option>
-        <option value='Xbox'>Xbox</option>
-        <option value='famicon'>ファミコン</option>
-        <option value='other'>その他</option>
-        </select><br>
-        アイコン<br>
-        <input type="file" name="image"><br>
-        サンプル画像<br>
-        <input type="file" name="sample1"><br>
-        <input type="file" name="sample2"><br>
-        <input type="file" name="sample3"><br>
-        概要<br>
-        <textarea name="summary" rows="5" cols="40"></textarea><br>
-        <input type=submit value="登録" class="manager_button"><br>
+        <h1>商品登録</h1><br>
+        <form action="game_add_complete.php" method="post" enctype="multipart/form-data" class=add_game>
 
-  
-    </form>    
+            <label>ゲーム名</label><br>
+            <input type=text name=game_name class=login_text required><br>
+            <label>料金</label><br>
+            <input type=text name=game_price class=login_text placeholder="￥" required><br>
+            <label>機種</label><br>
+            <select name='model' class=game_list>
+                <option value='switch'>Switch</option>
+                <option value='PS'>PS(プレイステーション)</option>
+                <option value='wii'>Wii</option>
+                <option value='64'>64</option>
+                <option value='Xbox'>Xbox</option>
+                <option value='famicon'>ファミコン</option>
+                <option value='other'>その他</option>
+            </select><br>
+            <label>ジャンル</label><br>
+            <select name='game_genre' class=game_list>
+                <option value='RPG'>RPG</option>
+                <option value='action'>アクション</option>
+                <option value='adventure'>アドベンチャー</option>
+                <option value='simulation'>シュミレーション</option>
+                <option value='格闘'>格闘</option>
+                <option value='famicon'>音楽(リズム)</option>
+                <option value='other'>その他</option>
+            </select><br>
+            <label>アイコン</label><br>
+            <input type="file" name="image" class=game_icon required><br>
+            <label>サンプル画像</label><br>
+            <input type="file" name="sample1" class=game_icon required><br>
+            <input type="file" name="sample2" class=game_icon><br>
+            <input type="file" name="sample3" class=game_icon><br>
+            <label>概要</label><br>
+            <textarea name="summary" rows="5" cols="40" class=game_summary required></textarea><br>
+            <input type=submit value="登録" class="add_button"><br>
+        </form>
     </main>
 </body>
 
