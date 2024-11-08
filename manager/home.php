0b6da1b1274ad53e81475bfe85120a91be31395c
Soyama1127 <2301017@s.asojuku.ac.jp>
2024-11-08 17:00:18 +0900

    なんかすごいことなってた

--- manager/home.php
+++ manager/home.php
@@ -14,19 +14,6 @@
     <header>
         GAMESoya管理者
     </header>
-<<<<<<< HEAD
-    <h1>管理者画面</h1>
-    <form action="login.php" method="post"><input name="0"value="ログアウト"></form>
-    <form action="game_add.php" method="post"><input name="a" type="submit" value="商品登録"style="width:300px;height:50px"></form>
-    <form action="game_update.php" method="post"><input name="b" type="submit" value="商品更新"style="width:300px;height:50px"></form>
-    <form action="game_delete.php" method="post"><input name="c" type="submit" value="商品削除"style="width:300px;height:50px"></form>
-    <form action="user_delete.php" method="post"><input name="d" type="submit" value="ユーザー削除"style="width:300px;height:50px"></form>
-    <form action="pr_approval.php" method="post"><input name="e" type="submit" value="ユーザーPR"style="width:300px;height:50px"></form>
-    <form action="game_stock.php" method="post"><input name="f" type="submit" value="在庫管理"style="width:300px;height:50px"></form>
-</body>
-</html>
-=======
->>>>>>> 15be628d4b600f252fa5b8aacc6dbb323d0e5bfb
 
     <h1>ホーム</h1>
 
