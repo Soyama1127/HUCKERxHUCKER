91552c7fbccead4763e1347392ea547af05dae83
Soyama1127 <2301017@s.asojuku.ac.jp>
2024-11-09 13:45:21 +0900

    いろいろ試した

--- user/home.php
+++ user/home.php
@@ -5,15 +5,51 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./../css/user.css">
+    <link rel="stylesheet" href="./../css/bootstrap.css">
+    <script src="./../js/bootstrap.js"></script>
     <title>ホーム画面</title>
 </head>
 
 <body>
     <header>
-        GAMESoya
+        <div class="header-content">
+            <div class="logo">
+                <img src='./../img/GAMESoya.PNG' width="150px" alt="Logo">
+            </div>
+
+            <div class="right-section">
+                <a href="search.php" class="button">
+                    <img src="./../img/search.webp" alt="Search">
+                </a>
+                <a href="cart.php" class="button">
+                    <img src="./../img/cart.png" alt="Cart">
+                </a>
+                <a href="account.php" class="button">
+                    アカウント
+                </a>
+            </div>
+        </div>
     </header>
     <main>
-        画面a
+        <div class="container">
+            <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img src="./../img/gamesoya home pic.png" class="d-block w-100" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="./../img/gamesoya home pic.png" class="d-block w-100" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="./../img/gamesoya home pic.png" class="d-block w-100" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="./../img/gamesoya home pic.png" class="d-block w-100" alt="...">
+                    </div>
+                </div>
+                </button>
+            </div>
+        </div>
     </main>
 </body>
 
