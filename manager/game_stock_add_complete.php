20ad41d616008976022d522153d5b37763389e50
yuki2004831 <ubei138sy@gmail.com>
2024-11-15 12:35:39 +0900

    wakan

--- manager/game_stock_add_complete.php
+++ manager/game_stock_add_complete.php
@@ -25,9 +25,14 @@
                 $sql = $pdo->prepare('updata gamesoya  set  game_stock=? where game_id = ?');
                 $sql->execute([$add,$game_id]);
                 ?>
+                <form action="game_stock.php" method="post">
+                    <input name="1" type="submit" value="続けて補充"style="width:300px;height:50px">
+                </form>
+                <form action="home.php" method="post">
+                    <input name="2" type="submit" value="ホームに戻る"style="width:300px;height:50px">
+                </form>
     </main>
-    <form action="game_stock.php" method="post"><input name="1" type="submit" value="続けて補充"style="width:300px;height:50px"></form>
-    <form action="home.php" method="post"><input name="2" type="submit" value="ホームに戻る"style="width:300px;height:50px"></form>
+    
 </body>
 
 </html>
