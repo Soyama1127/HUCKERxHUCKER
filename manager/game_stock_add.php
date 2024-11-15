63894f3c80b79edb3a45cb8e2b10c125b041a9ac
Soyama1127 <2301017@s.asojuku.ac.jp>
2024-11-15 13:51:22 +0900

    ごめん

--- manager/game_stock_add.php
+++ manager/game_stock_add.php
@@ -17,28 +17,24 @@
     <br>
     <br>
     <?php
-    
-    $pdo = new PDO(
-                    'mysql:host=mysql309.phy.lolipop.lan;dbname=LAA1553864-gamesoya;',
-                    'LAA1553864',
-                    'Pass1127'
-                );
-                $game_id = $_POST['game_id'];
-                $sql = $pdo->prepare('select * from game where game_id = ?');
-        $sql->execute([$game_id]);
-        foreach ($sql as $row): ?>
-            <?= $row['game_id'] ?>
-            <?= $row['game_name'] ?>
-            <?= $row['game_genre'] ?>
-            <?= $row['game_model'] ?>
-            <?= $row['game_price'] ?>
-      
-            <form action=game_stock_add_complete method='post'>
-            <input type='hidden' name=game_id value=<?=$row['game_id']?>>
+
+    $pdo = new PDO('mysql:host=mysql309.phy.lolipop.lan;dbname=LAA1553864-gamesoya;', 'LAA1553864', 'Pass1127');
+    $game_id = $_POST['game_id'];
+    $sql = $pdo->prepare('select * from game where game_id = ?');
+    $sql->execute([$game_id]);
+    foreach ($sql as $row): ?>
+        <?= $row['game_id'] ?>
+        <?= $row['game_name'] ?>
+        <?= $row['game_genre'] ?>
+        <?= $row['game_model'] ?>
+        <?= $row['game_price'] ?>
+
+        <form action=game_stock_add_complete method='post'>
+            <input type='hidden' name=game_id value=<?= $row['game_id'] ?>>
             <input type=number name=add method=post>
             <input type=submit value="補充" class="manager_button"><br>
-            </form>
-            <?php endforeach ?>
+        </form>
+    <?php endforeach ?>
     <script src="./../js/manager.js"></script>
 </body>
 
--- manager/game_stock_add_complete.php
+++ manager/game_stock_add_complete.php
@@ -15,24 +15,20 @@
     <main>
         <h1>在庫を補充しました</h1>
         <?php
-        $pdo = new PDO(
-                    'mysql:host=mysql309.phy.lolipop.lan;dbname=LAA1553864-gamesoya;',
-                    'LAA1553864',
-                    'Pass1127'
-                );
-                $game_id = $_POST['game_id'];
-                $add=$_POST['add'];
-                $sql = $pdo->prepare('update  game  set  game_stock=? where game_id = ?');
-                $sql->execute([$add,$game_id]);
-                ?>
-                <form action="game_stock.php" method="post">
-                    <input name="1" type="submit" value="続けて補充"style="width:300px;height:50px">
-                </form>
-                <form action="home.php" method="post">
-                    <input name="2" type="submit" value="ホームに戻る"style="width:300px;height:50px">
-                </form>
+        $pdo = new PDO('mysql:host=mysql309.phy.lolipop.lan;dbname=LAA1553864-gamesoya;', 'LAA1553864', 'Pass1127');
+        $game_id = $_POST['game_id'];
+        $add = $_POST['add'];
+        $sql = $pdo->prepare('update  game  set  game_stock=? where game_id = ?');
+        $sql->execute([$add, $game_id]);
+        ?>
+        <form action="game_stock.php" method="post">
+            <input name="1" type="submit" value="続けて補充" style="width:300px;height:50px">
+        </form>
+        <form action="home.php" method="post">
+            <input name="2" type="submit" value="ホームに戻る" style="width:300px;height:50px">
+        </form>
     </main>
-    
+
 </body>
 
 </html>
