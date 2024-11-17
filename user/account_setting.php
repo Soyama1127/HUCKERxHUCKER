ed481aaed56f218fc4b8caa837fd04f8b435ea83
Soyama1127 <2301017@s.asojuku.ac.jp>
2024-11-17 23:25:19 +0900

    戻るボタンの追加

--- user/account_setting.php
+++ user/account_setting.php
@@ -14,7 +14,7 @@ session_start();
 
 <body>
     <header>
-        <button class='back-btn' onclick='window.history.back();'>＜　アカウント設定</button>
+        <button class='back-btn' onclick='window.history.back();'>＜</button>
         <img src='./../img/GAMESoya.PNG' height="80px">
     </header>
     <main>
