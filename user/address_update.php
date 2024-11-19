cf185a624a06ec34a716437a2257383cca10a3f9
ishihara0212 <2301002@s.asojuku.ac.jp>
2024-11-19 12:19:41 +0900

    住所

--- user/address_update.php
+++ user/address_update.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 $pdo = new PDO('mysql:host=mysql309.phy.lolipop.lan;dbname=LAA1553864-gamesoya;', 'LAA1553864', 'Pass1127');
 $last_name = $_POST['last_name'];
 $first_name = $_POST['first_name'];
@@ -10,6 +11,6 @@ $city = $_POST['city'];
 $house_number = $_POST['house_number'];
 $house = $_POST['house'];
 $sql = $pdo->prepare('UPDATE `user` SET `last_name` = ?, `first_name` = ?, `last_namekana` = ?, `first_namekana` = ?, `post_code` = ?, `state` = ?, `city` = ?, `house_number` = ?, `house` = ? WHERE `user`.`user_id` = ?;');
-        $sql->execute([$last_name, $first_name, $last_namekana, $first_namekana, $post_code, $state, $city, $house_number, $house]);
+        $sql->execute([$last_name, $first_name, $last_namekana, $first_namekana, $post_code, $state, $city, $house_number, $house, $_SESSION['cart_id']]);
 header("Location: account_setting.php");
 ?>
