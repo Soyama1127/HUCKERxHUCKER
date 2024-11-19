823c6fdfe0ab9cab5f29394b30f2e83527c037ec
yuki2004831 <ubei138sy@gmail.com>
2024-11-19 12:37:32 +0900

    try

--- user/destroy_card.php
+++ user/destroy_card.php
@@ -7,7 +7,8 @@
     $card_text=$_POST['card_text'];
     $security=$_POST['security'];
     $user_id=$_SESSION['user_id'] ;
-    $sql = $pdo->prepare("INSERT INTO `creditcard` (`user_id`, `card_number`, `date_mounth`, `date_year`, `card_name`, `seurity_card`) VALUES (?, ?, ?, ?, ?, ?);");
+    $sql = $pdo->prepare("UPDATE `creditcard` SET `card_number` = ?, `date_mounth` = ?, `date_year` = ?, `card_name` = ?, `seurity_card` = ? WHERE `creditcard`.`user_id` = ?;
+ ");
     $sql->execute([$user_id,$card_id,$card_month,$card_year,$card_text,$security]);
     header("Location: account_setting.php");
 ?>
