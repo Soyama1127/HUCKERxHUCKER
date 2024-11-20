c093dc6f52cc8644aef9a1ecc044dc0108886fff
Soyama1127 <2301017@s.asojuku.ac.jp>
2024-11-20 12:31:27 +0900

    うーん...

--- user/update_card.php
+++ user/update_card.php
@@ -7,6 +7,6 @@ $card_year = $_POST['card_year'];
 $card_text = $_POST['card_text'];
 $security = $_POST['security'];
 $user_id = $_SESSION['cart_id'];
-$sql = $pdo->prepare("UPDATE `creditcard` SET `card_number` = ?, `date_mounth` = ?, `date_year` = ?, `card_name` = ?, `seurity_card` = ? WHERE `creditcard`.`user_id` = ?;");
+$sql = $pdo->prepare("UPDATE `creditcard` SET `card_number` = ?, `date_month` = ?, `date_year` = ?, `card_name` = ?, `seurity_card` = ? WHERE `creditcard`.`user_id` = ?;");
 $sql->execute([$card_id, $card_month, $card_year, $card_text, $security, $user_id]);
 header("Location: account_setting.php");
