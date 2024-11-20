3fb2bea93e14a56bd148df56f578f62eb7f95d4e
Soyama1127 <2301017@s.asojuku.ac.jp>
2024-11-20 12:44:00 +0900

    まじか

--- user/update_card.php
+++ user/update_card.php
@@ -4,9 +4,9 @@ $pdo = new PDO('mysql:host=mysql309.phy.lolipop.lan;dbname=LAA1553864-gamesoya;'
 $card_id = $_POST['card_id'];
 $card_month = $_POST['card_month'];
 $card_year = $_POST['card_year'];
-$card_text = $_POST['card_text'];
+$card_text = $_POST['card_name'];
 $security = $_POST['security'];
 $user_id = $_SESSION['cart_id'];
-$sql = $pdo->prepare("UPDATE `creditcard` SET `card_number` = ?, `date_month` = ?, `date_year` = ?, `card_name` = ?, `seurity_card` = ? WHERE `creditcard`.`user_id` = ?;");
+$sql = $pdo->prepare("UPDATE `creditcard` SET `card_number` = ?, `date_month` = ?, `date_year` = ?, `card_name` = ?, `security_card` = ? WHERE `creditcard`.`user_id` = ?;");
 $sql->execute([$card_id, $card_month, $card_year, $card_text, $security, $user_id]);
 header("Location: account_setting.php");
