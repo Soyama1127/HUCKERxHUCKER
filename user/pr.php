7973955804b53944c6abb7b6ea9fb02e43433900
yuki2004831 <ubei138sy@gmail.com>
2024-11-22 11:57:26 +0900

    一旦

--- user/pr.php
+++ user/pr.php
@@ -1,3 +1,6 @@
+<?php
+    session_start();
+ ?>
 <!DOCTYPE html>
 <html lang="ja">
 
@@ -10,11 +13,25 @@
 
 <body>
     <header>
-        <button class='back-btn' onclick="location.href='game.php'">＜</button>
+        <button class='back-btn' onclick="location.href='game.php'"></button>
         <img src='./../img/GAMESoya.PNG' height="80px">
     </header>
     <main>
-        画面
+        <form action="pr_request" method="post">
+            <input type="submit" value="PRを作成">
+        </form>
+        <h1>ユーザーPR</h1>
+        <?php
+        $pdo = new PDO('mysql:host=mysql309.phy.lolipop.lan;dbname=LAA1553864-gamesoya;', 'LAA1553864', 'Pass1127');
+        $_POST['game_id'];
+        $sql = $pdo->prepare('select * from userpr inner join user on user.user_id = userpr.user_id where user_id=?');
+        $sql->execute([$_SESSION['user_id']]);
+        foreach ($sql as $row): 
+              $row['pr_crip']?> <br>
+           <? $row['user_name']?><br>
+           <? $row['pr_content']?><br>
+        <?endforeach?>
+
     </main>
 </body>
 
