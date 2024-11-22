4f1ebf49555fef58591f5426d6f791fd3ce1eba5
yuki2004831 <ubei138sy@gmail.com>
2024-11-22 13:41:57 +0900

    一旦

--- user/pr_request.php
+++ user/pr_request.php
@@ -14,7 +14,16 @@
         <img src='./../img/GAMESoya.PNG' height="80px">
     </header>
     <main>
-        画面
+        <?php
+    $pdo = new PDO('mysql:host=mysql309.phy.lolipop.lan;dbname=LAA1553864-gamesoya;', 'LAA1553864', 'Pass1127');
+        $sql = $pdo->prepare('select game_name from game where game_id=?');
+        $sql->execute([$_SESSION['game_id']]);
+        ?>
+    <label>PR文</label><br>
+    <input type=textarea name=prcontent rows="5" cols="40" required>
+    <label>サンプル画像</label><br>
+    <input type="file" name="pr_movie" required><br>
+    <input type=submit value="申請" ><br>
     </main>
 </body>
 
