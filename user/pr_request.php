373d2b761a0c0ddbc5a758bc964d9e9e6819c0f5
Soyama1127 <2301017@s.asojuku.ac.jp>
2024-11-22 14:15:21 +0900

    マジごめん

--- user/pr_request.php
+++ user/pr_request.php
@@ -21,19 +21,8 @@ session_start();
         $pdo = new PDO('mysql:host=mysql309.phy.lolipop.lan;dbname=LAA1553864-gamesoya;', 'LAA1553864', 'Pass1127');
         $sql = $pdo->prepare('select * from game where game_id=?');
         $sql->execute([$_SESSION['game_id']]);
-<<<<<<< HEAD
-        foreach ($sql as $row):?> 
-            <h2><?=$row['game_name']?> </h2><br>
-            
-         <?php endforeach ?>
-    <label>PR文</label><br> 
-    <textarea name=prcontent rows="5" cols="40" required></textarea>
-    <label>サンプル画像</label><br>
-    <input type="file" name="pr_movie" required><br>
-    <input type=submit value="申請" ><br>
-=======
         foreach ($sql as $row): ?>
-            <?= $row['game_name'] ?> <br>
+            <h2><?= $row['game_name'] ?></h2>
 
         <?php endforeach ?>
         <label>PR文</label><br>
@@ -41,7 +30,6 @@ session_start();
         <label>サンプル画像</label><br>
         <input type="file" name="pr_movie" required><br>
         <input type=submit value="申請"><br>
->>>>>>> 2c9a95835b0062238bca5574db495bed0b3b5899
     </main>
 </body>
 
