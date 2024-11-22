3924f61fa940858f861652a2139b5a3adb3f7c41
ishihara0212 <2301002@s.asojuku.ac.jp>
2024-11-22 13:14:39 +0900

    id

--- user/update_id.php
+++ user/update_id.php
@@ -1,8 +1,8 @@
 <?php
 session_start();
 $pdo = new PDO('mysql:host=mysql309.phy.lolipop.lan;dbname=LAA1553864-gamesoya;', 'LAA1553864', 'Pass1127');
-$login_id = $_POST['login_id'];
-$sql = $pdo->prepare('UPDATE `user` SET `login_id` = ? WHERE `user`.`user_id` = ?;');
-$sql->execute([$login_id,$_SESSION['user_id']]);
+$user_id = $_POST['user_id'];
+$sql = $pdo->prepare('UPDATE `user` SET `user_id` = ? WHERE `user`.`user_id` = ?;');
+$sql->execute([$user_id,$_SESSION['user_id']]);
 "alert('ログインIDを登録しました')";
 header("Location: account_setting.php");
--- user/update_pass.php
+++ user/update_pass.php
@@ -3,6 +3,6 @@ session_start();
 $pdo = new PDO('mysql:host=mysql309.phy.lolipop.lan;dbname=LAA1553864-gamesoya;', 'LAA1553864', 'Pass1127');
 $user_password = $_POST['user_password'];
 $sql = $pdo->prepare('UPDATE `user` SET `user_password` = ? WHERE `user`.`user_id` = ?;');
-$sql->execute([$user_password,$login_id,$_SESSION['user_id']]);
+$sql->execute([$user_password,$_SESSION['user_id']]);
 "alert('パスワードを登録しました')";
 header("Location: account_setting.php");
