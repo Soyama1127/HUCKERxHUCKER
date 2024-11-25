7bb6bc630ebc6dad4bde5722040a53ec221b4ef1
Soyama1127 <2301017@s.asojuku.ac.jp>
2024-11-25 10:34:15 +0900

    a

--- user/destroy_gamesoya.php
+++ user/destroy_gamesoya.php
@@ -1,12 +1,11 @@
 <?php
 session_start();
 $pdo = new PDO('mysql:host=mysql309.phy.lolipop.lan;dbname=LAA1553864-gamesoya;', 'LAA1553864', 'Pass1127');
-$POST['pr_content'];
-$POSR['pr_movie'];
-$count=0;
-$_SESSION['user_id'];
-$_SESSION['game_id'];
-$sql = $pdo->prepare("insert into userpr ('pr_id, user_id',game_id,pr_content,pr_clip,) value ('?,?,?,?,?') ");
-$sql->execute([$_SESSION['count,user_id,game_id,pr_content,pr_movie']]);
+$pr_content=$_POST['pr_content'];
+$pr_movie-$_POST['pr_movie'];
+$user_id=$_SESSION['user_id'];
+$game_id=$_SESSION['game_id'];
+$sql = $pdo->prepare('insert into userpr (user_id, game_id, pr_content, pr_clip) values (?,?,?,?)');
+$sql->execute(['$user_id,$game_id,$pr_content,$pr_movie']);
 header("Location: pr.php");
 ?>
