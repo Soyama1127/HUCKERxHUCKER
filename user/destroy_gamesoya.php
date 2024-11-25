5b00e1a84ae49a3407e11fcac1b109c49d5b18a6
Soyama1127 <2301017@s.asojuku.ac.jp>
2024-11-25 11:05:06 +0900

    ユーザーPR完成

--- user/destroy_gamesoya.php
+++ user/destroy_gamesoya.php
@@ -5,14 +5,14 @@ $pr_content=$_POST['pr_content'];
 $user_id=$_SESSION['user_id'];
 $game_id=$_SESSION['game_id'];
 if (
-    is_uploaded_file($_FILES['movie']['tmp_name']) 
+    is_uploaded_file($_FILES['pr_movie']['tmp_name']) 
 ){
-    $image_path = './../manager/pr_movie/' . basename($_FILES['movie']['name']);
+    $image_path = './../manager/pr_movie/' . basename($_FILES['pr_movie']['name']);
 
-$pr_movie = basename($_FILES['movie']['name']);
+$pr_movie = basename($_FILES['pr_movie']['name']);
 
 if (
-    move_uploaded_file($_FILES['movie']['tmp_name'], $image_path)
+    move_uploaded_file($_FILES['pr_movie']['tmp_name'], $image_path)
 ){
 
 $sql = $pdo->prepare('insert into userpr (user_id, game_id, pr_content, pr_clip) values (?,?,?,?)');
--- user/pr_request.php
+++ user/pr_request.php
@@ -18,19 +18,19 @@ session_start();
     </header>
     <main>
         <form action="destroy_gamesoya" method="post" enctype="multipart/form-data">
-        <?php
-        $pdo = new PDO('mysql:host=mysql309.phy.lolipop.lan;dbname=LAA1553864-gamesoya;', 'LAA1553864', 'Pass1127');
-        $sql = $pdo->prepare('select * from game where game_id=?');
-        $sql->execute([$_SESSION['game_id']]);
-        foreach ($sql as $row):?> 
-            <h2><?=$row['game_name']?> </h2><br>
-            
-         <?php endforeach ?>
-    <label>PR文</label><br> 
-    <textarea name=pr_content rows="5" cols="40" required></textarea>
-    <label>サンプル画像</label><br>
-    <input type="file" name="pr_movie" required><br>
-    <input type=submit value="申請" ><br>
+            <?php
+            $pdo = new PDO('mysql:host=mysql309.phy.lolipop.lan;dbname=LAA1553864-gamesoya;', 'LAA1553864', 'Pass1127');
+            $sql = $pdo->prepare('select * from game where game_id=?');
+            $sql->execute([$_SESSION['game_id']]);
+            foreach ($sql as $row): ?>
+                <h2><?= $row['game_name'] ?> </h2><br>
+
+            <?php endforeach ?>
+            <label>PR文</label><br>
+            <textarea name=pr_content rows="5" cols="40" required></textarea>
+            <label>サンプル画像</label><br>
+            <input type="file" name="pr_movie" required><br>
+            <input type=submit value="申請"><br>
         </form>
     </main>
 </body>
