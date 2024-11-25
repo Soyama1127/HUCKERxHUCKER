b74264c18dc14e79e0b52810112867ba2822a505
ishihara0212 <2301002@s.asojuku.ac.jp>
2024-11-25 10:22:01 +0900

    よろ

--- user/search.php
+++ user/search.php
@@ -17,23 +17,23 @@
     
         <input type="textbox" name="game_name" placeholder="検索" style="width:300px;height:20px"><form action="search_result.php" method="post"><button type="submit">検索</button></form><br>
         機種<hr width="500">
-        <input type="radio" name="game_model" value="1" checked>Switch
-        <input type="radio" name="game_model" value="2">PS(プレステーション)<br>
-        <input type="radio" name="game_model" value="3">Wii
-        <input type="radio" name="game_model" value="4">DS<br>
-        <input type="radio" name="game_model" value="5">3DS
-        <input type="radio" name="game_model" value="6">64<br>
-        <input type="radio" name="game_model" value="7">xbox
-        <input type="radio" name="game_model" value="8">ファミコン<br>
-        <input type="radio" name="game_model" value="9">その他<br>
+        <input type="radio" name="game_model" value="Switch">Switch
+        <input type="radio" name="game_model" value="PS(プレステーション)">PS(プレステーション)<br>
+        <input type="radio" name="game_model" value="Wii">Wii
+        <input type="radio" name="game_model" value="DS">DS<br>
+        <input type="radio" name="game_model" value="3DS">3DS
+        <input type="radio" name="game_model" value="64">64<br>
+        <input type="radio" name="game_model" value="xbox">xbox
+        <input type="radio" name="game_model" value="ファミコン">ファミコン<br>
+        <input type="radio" name="game_model" value="その他">その他<br>
         ジャンル<br>
-        <input type="radio" name="game_genre" value="1" checked>RPG
-        <input type="radio" name="game_genre" value="2">アクション<br>
-        <input type="radio" name="game_genre" value="3">アドベンチャー
-        <input type="radio" name="game_genre" value="4">シュミレーション<br>
-        <input type="radio" name="game_genre" value="5">格闘
-        <input type="radio" name="game_genre" value="6">音楽<br>
-        <input type="radio" name="game_genre" value="7">その他
+        <input type="radio" name="game_genre" value="RPG">RPG
+        <input type="radio" name="game_genre" value="アクション">アクション<br>
+        <input type="radio" name="game_genre" value="アドベンチャー">アドベンチャー
+        <input type="radio" name="game_genre" value="シュミレーション">シュミレーション<br>
+        <input type="radio" name="game_genre" value="格闘">格闘
+        <input type="radio" name="game_genre" value="音楽">音楽<br>
+        <input type="radio" name="game_genre" value="その他">その他
 </body>
 
 </html>
