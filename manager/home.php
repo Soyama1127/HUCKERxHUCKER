ea9e1647c0e80b5a383ad292a3ad12781a7c97a0
Soyama1127 <2301017@s.asojuku.ac.jp>
2024-11-28 02:40:46 +0900

    はぁ

--- manager/home.php
+++ manager/home.php
@@ -5,8 +5,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./../css/manager.css">
-    <link rel="stylesheet" href="./../css/bootstrap.css">
-    <script src="./../js/bootstrap.js"></script>
     <title>管理者ホーム</title>
 </head>
 
@@ -14,59 +12,59 @@
     <header>
         GAMESoya管理者
     </header>
+    <main>
+        <h1>ホーム</h1>
 
-    <h1>ホーム</h1>
+        <form action="login.php" method="post" class="logout" onsubmit="return confirmLogout();">
 
-    <form action="login.php" method="post" class="logout" onsubmit="return confirmLogout();">
+            <input type="submit" value="ログアウト" class="logout_button">
 
-        <input type="submit" value="ログアウト" class="logout_button">
+        </form>
+        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-5 mt-5">
 
-    </form>
-    <br><br>
-    <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-5 mt-5">
+            <!-- 商品登録ボタン -->
+            <div class="col">
+                <form action="game_add.php" method="post">
+                    <input type="submit" value="商品登録" class="home_button">
+                </form>
+            </div>
 
-        <!-- 商品登録ボタン -->
-        <div class="col">
-            <form action="game_add.php" method="post">
-                <input type="submit" value="商品登録" class="home_button">
-            </form>
+            <!-- 商品更新ボタン -->
+            <div class="col">
+                <form action="game_choose.php" method="post">
+                    <input type="submit" value="商品更新" class="home_button">
+                </form>
+            </div>
+
+            <!-- 商品削除ボタン -->
+            <div class="col">
+                <form action="game_delete.php" method="post">
+                    <input type="submit" value="商品削除" class="home_button">
+                </form>
+            </div>
+
+            <!-- ユーザー削除ボタン -->
+            <div class="col">
+                <form action="user_delete.php" method="post">
+                    <input type="submit" value="ユーザー削除" class="home_button">
+                </form>
+            </div>
+
+            <!-- ユーザーPRボタン -->
+            <div class="col">
+                <form action="pr_approval.php" method="post">
+                    <input type="submit" value="ユーザーPR" class="home_button">
+                </form>
+            </div>
+
+            <!-- 在庫管理ボタン -->
+            <div class="col">
+                <form action="game_stock.php" method="post">
+                    <input type="submit" value="在庫管理" class="home_button">
+                </form>
+            </div>
         </div>
-
-        <!-- 商品更新ボタン -->
-        <div class="col">
-            <form action="game_choose.php" method="post">
-                <input type="submit" value="商品更新" class="home_button">
-            </form>
-        </div>
-
-        <!-- 商品削除ボタン -->
-        <div class="col">
-            <form action="game_delete.php" method="post">
-                <input type="submit" value="商品削除" class="home_button">
-            </form>
-        </div>
-
-        <!-- ユーザー削除ボタン -->
-        <div class="col">
-            <form action="user_delete.php" method="post">
-                <input type="submit" value="ユーザー削除" class="home_button">
-            </form>
-        </div>
-
-        <!-- ユーザーPRボタン -->
-        <div class="col">
-            <form action="pr_approval.php" method="post">
-                <input type="submit" value="ユーザーPR" class="home_button">
-            </form>
-        </div>
-
-        <!-- 在庫管理ボタン -->
-        <div class="col">
-            <form action="game_stock.php" method="post">
-                <input type="submit" value="在庫管理" class="home_button">
-            </form>
-        </div>
-    </div>
+    </main>
     <script src="./../js/manager.js"></script>
 </body>
 
